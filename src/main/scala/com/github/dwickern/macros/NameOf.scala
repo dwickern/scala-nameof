e39f7e0010bcaedab5178712f1157ca9a9945f25
Derek Wickern <dwickern@gmail.com>
2016-09-22 15:08:23 -0700

    add inline documentation

--- src/main/scala/com/github/dwickern/macros/NameOf.scala
+++ src/main/scala/com/github/dwickern/macros/NameOf.scala
@@ -3,7 +3,38 @@ package com.github.dwickern.macros
 object NameOf {
   import scala.language.experimental.macros
 
-  def nameOf(x: Any): String = macro NameOfImpl.nameOfImpl
-  def nameOf[T](f: T => Any): String = macro NameOfImpl.nameOfMemberImpl
-  def nameOfType[T]: String = macro NameOfImpl.nameOfTypeImpl[T]
+  /**
+    * Obtain an identifier name as a constant string.
+    *
+    * Example usage:
+    * {{{
+    *   val amount = 5
+    *   nameOf(amount) => "amount"
+    * }}}
+    */
+  def nameOf(expr: Any): String = macro NameOfImpl.nameOf
+
+  /**
+    * Obtain an identifier name as a constant string.
+    *
+    * This overload can be used to access an instance method without having an instance of the type.
+    *
+    * Example usage:
+    * {{{
+    *   class Person(val name: String)
+    *   nameOf[Person](_.name) => "name"
+    * }}}
+    */
+  def nameOf[T](expr: T => Any): String = macro NameOfImpl.nameOf
+
+  /**
+    * Obtain a type's name as a constant string.
+    *
+    * Example usage:
+    * {{{
+    *   nameOfType[String] => "String"
+    *   nameOfType[fully.qualified.ClassName] => "ClassName"
+    * }}}
+    */
+  def nameOfType[T]: String = macro NameOfImpl.nameOfType[T]
 }
--- src/main/scala/com/github/dwickern/macros/NameOfImpl.scala
+++ src/main/scala/com/github/dwickern/macros/NameOfImpl.scala
@@ -4,7 +4,7 @@ import scala.annotation.tailrec
 import scala.reflect.macros._
 
 private[macros] object NameOfImpl {
-  def nameOfImpl(c: Context)(x: c.Expr[Any]): c.Expr[String] = {
+  def nameOf(c: Context)(expr: c.Expr[Any]): c.Expr[String] = {
     import c.universe._
 
     @tailrec def extract(tree: c.Tree): c.Name = tree match {
@@ -14,17 +14,16 @@ private[macros] object NameOfImpl {
       case Block(_, expr) => extract(expr)
       case Apply(func, _) => extract(func)
       case TypeApply(func, _) => extract(func)
-      case _ => c.abort(c.enclosingPosition, s"Unsupported expression: $x")
+      case _ => c.abort(c.enclosingPosition, s"Unsupported expression: $expr")
     }
 
-    val name = extract(x.tree).decoded
+    val name = extract(expr.tree).decoded
     reify {
       c.Expr[String] { Literal(Constant(name)) }.splice
     }
   }
 
-  def nameOfMemberImpl(c: Context)(f: c.Expr[Any]): c.Expr[String] = nameOfImpl(c)(f)
-  def nameOfTypeImpl[T](c: Context)(implicit tag: c.WeakTypeTag[T]): c.Expr[String] = {
+  def nameOfType[T](c: Context)(implicit tag: c.WeakTypeTag[T]): c.Expr[String] = {
     import c.universe._
     val name = showRaw(tag.tpe.typeSymbol.name)
     reify {
